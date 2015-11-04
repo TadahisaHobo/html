1b85a85d733a9c52cde59fb8aa9ed8d7274b5e05
tadahisa hobo <hobo@ipservice.jp>
2015-11-04 11:38:20 +0900

    test

---
+++ html/test.php
@@ -0,0 +1,57 @@
+<style type="text/css">
+    form#frm_a {
+        margin:10px 0; padding:0;
+    }
+    form#frm_a fieldset {
+        margin:0; padding:10px;
+        border:1px solid #6bba70;
+        background-color:inherit;
+        color:#fff;
+        font-size:12px;
+    }
+    form#frm_a legend {
+        margin:0 0 10px 0; padding:2px 5px;
+        color:#6bba70;
+        font-weight:bold;
+    }
+    form#frm_a p {
+        margin:0; padding:0 0 10px 0;
+        color:#666;
+    }
+    form#frm_a input.txt {
+        border:1px solid #cdeb8b;;
+        font-weight:bold;
+        padding:1px 3px;
+    }
+    form#frm_a input.submit {
+        border:3px double #cdeb8b;
+        background-color:#fff;
+        color:#6bba70;
+        font-weight:bold;
+    }
+</style>
+
+<form id="frm_a" action="#" onsubmit="alert('ƒTƒ“ƒvƒ‹‚Ì‚½‚ß‘—M‚µ‚Ü‚¹‚ñ'); return false;">
+    <fieldset>
+        <legend>‰ïˆõ“o˜^</legend>
+        <p>
+            <label for="lastname">: <input type="text" class="txt" id="lastname" name="lastname" size="10" /></label>
+            <label for="firstname">–¼: <input type="text" class="txt" id="firstname" name="firstname" size="10" /></label>
+        </p>
+        <p>
+            <label for="email">E-mail: <input type="text" class="txt" id="email" name="email" size="40" /></label>
+        </p>
+        <p>
+            ¶”NŒ“ú: ¼—ï 
+            <label for="birth_y"><input type="text" class="txt" id="birth_y" name="birth_y" size="4" maxlength="4" />”N</label>
+            <label for="birth_m"><input type="text" class="txt" id="birth_m" name="birth_m" size="2" maxlength="2" />Œ</label>
+            <label for="birth_d"><input type="text" class="txt" id="birth_d" name="birth_d" size="2" maxlength="2" />“ú</label>
+        </p>
+        <p>
+            «•Ê: 
+            <label for="sex1"><input type="radio" id="sex1" name="sex" value="male" />’j</label>
+            <label for="sex2"><input type="radio" id="sex2" name="sex" value="female" />—</label>
+        </p>
+        <input type="submit" class="submit" value="“o˜^‚·‚é" />
+    </fieldset>
+</form>
